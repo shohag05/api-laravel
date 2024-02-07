511b4e8c775419308accdd1cb0a574a119ba28b3
Shohag Hossain <santo.shohag.22@gmail.com>
2024-02-07 17:03:59 +0600

    update broken font url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,8 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Url not found -->
+        <!-- <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> -->
 
         <!-- Styles -->
         <style>
