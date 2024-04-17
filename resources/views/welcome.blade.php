478096773d752ac8c12ae117488eb129d2bb1abf
H A S A N <hasan25042019@gmail.com>
2024-04-17 16:43:38 +0600

    change to laravel change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Lravue</title>
+        <title>Lravue change</title>
 
         <!-- Fonts -->
         <!-- Url not found -->
